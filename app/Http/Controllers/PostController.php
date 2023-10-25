4ede695ca663dd04823e87e55122fcb83422d51a
okura0203 <akutagawa5802@gmail.com>
2023-10-25 10:57:24 +0900

    修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Cloudinary;  //use宣言するのを忘れずに
 
 class PostController extends Controller
 {
@@ -11,5 +12,24 @@ class PostController extends Controller
     {
         return view('posts/index')->with(['posts' => $post->getByLimit()]);//$postの中身を戻り値にする。
     }
+    
+    public function create()
+    {
+        return view('/posts/create');  //create.blade.phpを表示
+    }
+
+    public function store(Request $request, Post $post)
+    {
+        //cloudinaryへ画像を送信し、
+        $input = $request['post'];
+        if($request->file('image')){ //画像ファイルが送られた時だけ処理が実行される
+            $image_url = Cloudinary::upload($request->file('image')->getRealPath())->getSecurePath();
+            $input += ['image_url' => $image_url];
+        }
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+   
 }
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -40,3 +41,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/posts/create', [PostController::class, 'create']);  //投稿フォームの表示
+Route::post('/posts', [PostController::class, 'store']);  //画像を含めた投稿の保存処理
+Route::get('/posts/{post}', [PostController::class, 'show']); //投稿詳細画面の表示
